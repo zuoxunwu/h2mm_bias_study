8b05db1bbc4d63d192e44e03d7bf43b6c82389cf
Xunwu Zuo <zuoxunwu@gmail.com>
2021-09-28 11:56:55 +0200

    clean unneeded logs and outputs.

--- Backup/05282019/muPairs_mass/consthist.py
+++
@@ -1,9 +0,0 @@
-import ROOT as rt
-x = rt.RooRealVar("x", "x", -1, 1)
-uni = rt.RooUniform("uni","uni",rt.RooArgSet(x))
-hist_uni = uni.generate(rt.RooArgSet(x),1000000)
-xframe = x.frame(rt.RooFit.Title("uni"))
-hist_uni.plotOn(xframe)
-c = rt.TCanvas("c","c",800,400)
-xframe.Draw()
-c.SaveAs("uni.png")
